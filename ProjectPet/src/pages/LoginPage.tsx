e8ae39dbdd7146e3e6fb019b6a102bbd271074a1
brinersam <vladbudyachenko@mail.ru>
2025-06-07 20:42:50 +0300

    Валидация формы

--- ProjectPet/src/pages/LoginPage.tsx
+++ ProjectPet/src/pages/LoginPage.tsx
@@ -3,28 +3,76 @@ import {
   FormControl,
   Grid,
   IconButton,
-  Input,
   InputAdornment,
-  InputLabel,
-  Link,
   Paper,
+  TextField,
   Typography,
 } from "@mui/material";
 import { useState, type ReactNode } from "react";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 import { PATHS } from "../app/Paths";
 
-export default function LoginPage(): ReactNode {
+export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
+
+  type FormField = "email" | "password";
+  type FormState = Record<FormField, string>;
+  const [formData, setFormData] = useState<FormState>({
+    email: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState<FormState>({
+    email: "",
+    password: "",
+  });
+  const doesFormHaveErrors = Object.entries(formErrors).some(([key, value]) => {
+    return value;
+  });
+
+  const handleFormChange = (key: FormField, value: string) => {
+    if (key == "email") {
+      setFormData((x) => ({ ...x, email: value }));
+
+      if (!value.includes("@") && value) {
+        setFormErrors((x) => ({ ...x, email: "Email is invalid" }));
+      } else {
+        setFormErrors((x) => ({ ...x, email: "" }));
+      }
+
+      return;
+    }
+
+    if (key == "password") {
+      setFormData((x) => ({ ...x, password: value }));
+    }
+  };
+
+  const HandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (doesFormHaveErrors) return;
+
+    console.log("Form submitted:", formData);
+  };
+
+  function formErrorHtml(key: FormField): ReactNode {
+    if (formErrors[key])
+      return (
+        <Typography variant="caption" color="error">
+          {formErrors[key]}
+        </Typography>
+      );
+    return <></>;
+  }
+
   return <>{RenderHtml()}</>;
 
-  function RenderHtml(): ReactNode {
+  function RenderHtml() {
     return (
       <>
-        {BackToMainBtnHtml()}
+        <BackToMainBtnHtml />
         <Paper
           elevation={4}
           sx={{
@@ -36,154 +84,187 @@ export default function LoginPage(): ReactNode {
             maxHeight: "60rem",
           }}
         >
-          <Grid container sx={{ justifyContent: "center" }}>
-            {TitleHtml()}
-            {TextBoxHmtl("Email", 1)}
+          <Grid
+            component="form"
+            onSubmit={HandleSubmit}
+            container
+            sx={{ justifyContent: "center" }}
+          >
+            <TitleHtml />
+            {EmailBoxHtml("Email,", 1)}
             {PasswordBoxHtml(1)}
-            <Grid sx={{ width: "70%" }}>{ButtonHtml("LogIn")}</Grid>
-            {RegistrationSuggestionHtml()}
+            <Grid size={12} sx={{ justifyContent: "center" }}>
+              <ButtonHtml label="LogIn" />
+            </Grid>
+            <RegistrationSuggestionHtml />
           </Grid>
         </Paper>
       </>
     );
+  }
 
-    function BackToMainBtnHtml() {
-      return (
-        <Paper sx={{ width: "40%" }}>
-          <NavLink to={PATHS.Index}>
-            {ButtonHtml(
+  function BackToMainBtnHtml() {
+    return (
+      <Paper sx={{ width: "40%" }}>
+        <NavLink to={PATHS.Index}>
+          <ButtonHtml
+            label={
               <>
                 <ArrowBackIosIcon />
                 Back to Main
-              </>,
-              3
-            )}
-          </NavLink>
-        </Paper>
-      );
-    }
+              </>
+            }
+            elevation={3}
+          />
+        </NavLink>
+      </Paper>
+    );
+  }
 
-    function RegistrationSuggestionHtml() {
-      return (
-        <Grid container columnSpacing={1}>
-          <Typography variant="caption">Dont have an account?</Typography>
-          <Typography
-            sx={{ textDecoration: "underline" }}
-            display="inline"
-            variant="caption"
-            color="secondary"
-          >
-            <NavLink to={PATHS.Registration}>Create one.</NavLink>
-          </Typography>
-        </Grid>
-      );
-    }
-
-    function TitleHtml() {
-      return (
+  function RegistrationSuggestionHtml() {
+    return (
+      <Grid container columnSpacing={1}>
+        <Typography variant="caption">Dont have an account?</Typography>
         <Typography
-          variant="h4"
-          sx={{
-            marginBottom: "2rem",
-            display: { md: "flex" },
-            fontFamily: "monospace",
-            fontWeight: 700,
-            color: "inherit",
-          }}
+          sx={{ textDecoration: "underline" }}
+          display="inline"
+          variant="caption"
+          color="secondary"
         >
-          Login
+          <NavLink to={PATHS.Registration}>Create one.</NavLink>
         </Typography>
-      );
-    }
+      </Grid>
+    );
+  }
 
-    function ButtonHtml(label: string | ReactNode, elevation: number = 0) {
-      return (
-        <Paper
-          sx={{
-            margin: "1rem",
-            width: "100%",
-          }}
-          elevation={elevation}
-        >
-          <Button
-            sx={{
-              width: "100%",
-            }}
-          >
-            {label}
-          </Button>
-        </Paper>
-      );
-    }
+  function TitleHtml() {
+    return (
+      <Typography
+        variant="h4"
+        sx={{
+          marginBottom: "2rem",
+          display: { md: "flex" },
+          fontFamily: "monospace",
+          fontWeight: 700,
+          color: "inherit",
+        }}
+      >
+        Login
+      </Typography>
+    );
+  }
 
-    function TextBoxHmtl(label: string, elevation: number = 0) {
-      return (
-        <Paper
-          square={false}
-          elevation={elevation}
+  function ButtonHtml({
+    label,
+    elevation = 0,
+  }: {
+    label: string | ReactNode;
+    elevation?: number;
+  }) {
+    return (
+      <Paper
+        sx={{
+          margin: "1rem",
+          width: "100%",
+        }}
+        elevation={elevation}
+      >
+        <Button
+          type="submit"
+          disabled={doesFormHaveErrors}
           sx={{
-            padding: 1 + "%",
-            display: "flex",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <FormControl
-            sx={{
-              width: "100%",
-            }}
-            variant="filled"
-            size="small"
-          >
-            <InputLabel htmlFor={label}>{label}</InputLabel>
-            <Input id={label} type="text" />
-          </FormControl>
-        </Paper>
-      );
-    }
-
-    function PasswordBoxHtml(elevation: number = 0) {
-      return (
-        <Paper
-          square={false}
-          elevation={elevation}
-          sx={{
-            padding: 1 + "%",
-            display: "flex",
-            alignItems: "center",
             width: "100%",
           }}
         >
-          <FormControl
-            sx={{
-              width: "100%",
+          {label}
+        </Button>
+      </Paper>
+    );
+  }
+
+  function EmailBoxHtml(label: string, elevation: number = 0) {
+    return (
+      <Paper
+        square={false}
+        elevation={elevation}
+        sx={{
+          padding: 1 + "%",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <FormControl
+          sx={{
+            width: "100%",
+          }}
+          variant="filled"
+          size="small"
+        >
+          <TextField
+            placeholder="Email"
+            required
+            onChange={(event) => handleFormChange("email", event.target.value)}
+            id={label}
+            type="text"
+            helperText={formErrorHtml("email")}
+          />
+        </FormControl>
+      </Paper>
+    );
+  }
+
+  function PasswordBoxHtml(elevation: number = 0) {
+    return (
+      <Paper
+        square={false}
+        elevation={elevation}
+        sx={{
+          padding: 1 + "%",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <FormControl
+          sx={{
+            width: "100%",
+          }}
+          variant="filled"
+          size="small"
+        >
+          <TextField
+            onChange={(event) =>
+              handleFormChange("password", event.target.value)
+            }
+            required
+            placeholder="Password"
+            type={showPassword ? "text" : "password"}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword
+                          ? "hide the password"
+                          : "display the password"
+                      }
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <VisibilityOffIcon />
+                      ) : (
+                        <VisibilityIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
             }}
-            variant="filled"
-            size="small"
-          >
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              id="password"
-              type={showPassword ? "text" : "password"}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label={
-                      showPassword
-                        ? "hide the password"
-                        : "display the password"
-                    }
-                    onClick={() => setShowPassword(!showPassword)}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </Paper>
-      );
-    }
+          />
+        </FormControl>
+      </Paper>
+    );
   }
 }
