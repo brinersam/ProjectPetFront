626ccac8e5ed5a78671077e213453d3e00c82af1
brinersam <vladbudyachenko@mail.ru>
2025-07-28 20:19:06 +0300

    Ошибки

---
+++ ProjectPet/src/app/Helpers/Errors.ts
@@ -0,0 +1,10 @@
+export const errorMessages: Record<string,string> = {
+    "record.not.found" : "Не найдена запись",
+    "errCode" : "nuts"
+}
+
+export enum errorConsts
+{
+    Unknown = "Неизвестная ошибка",
+    Server = "Ошибка сервера"
+}
