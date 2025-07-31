b7c6f1ffcd3f32d0315d1c3eae4abc0560b4ff81
brinersam <vladbudyachenko@mail.ru>
2025-08-01 00:54:11 +0300

    Рефактор хелпер класса ошибок

--- ProjectPet/src/app/Helpers/ExceptionsHelper.tsx
+++ ProjectPet/src/app/Helpers/ExceptionsHelper.tsx
@@ -4,6 +4,7 @@ import type { Envelope } from "../../models/responses";
 import { errorConsts, errorMessages } from "./Errors";
 import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 import type { SerializedError } from "@reduxjs/toolkit";
+import type { Error as BackendError } from "../../models/responses";
 
 export default class ExceptionsHelper {
   static ToastError(
@@ -12,24 +13,34 @@ export default class ExceptionsHelper {
       | FetchBaseQueryError
       | SerializedError
       | Error
+      | Error[]
       | unknown,
     rethrow: boolean = true,
     errorPrefix: string | undefined = "Error: "
   ) {
     let errorMessage: any | undefined = undefined;
 
+    errorMessage ??= ExceptionsHelper.tryHandleUndefinedError(exception);
     errorMessage ??= ExceptionsHelper.tryHandleAxiosError(exception);
     errorMessage ??= ExceptionsHelper.tryHandleFetchBaseQueryError(exception);
+    errorMessage ??= ExceptionsHelper.tryHandleErrorArray(exception);
     errorMessage ??= exception;
 
     toast(errorPrefix + errorMessage);
     if (rethrow && exception instanceof Error) throw exception;
   }
 
+  static tryHandleUndefinedError(exception: any) {
+    if (exception) return undefined;
+
+    return errorConsts.Unknown;
+  }
+
   static tryHandleAxiosError(exception: any) {
     if (isAxiosError<Envelope<any>>(exception) == false) return undefined;
+    if (exception.response?.data?.errors == undefined) return undefined;
 
-    return exception.response?.data?.errors.map((x) => x.message).join("; ");
+    return formatErrorArray(exception.response?.data?.errors);
   }
 
   static tryHandleFetchBaseQueryError(exception: any) {
@@ -37,10 +48,19 @@ export default class ExceptionsHelper {
 
     const errorData = exception.data as Envelope<null> | undefined;
     if (!errorData || !errorData?.errors) return errorConsts.Server; // couldnt parse | no errors sent
-
-    const messages = errorData?.errors.map((err) => {
-      return errorMessages[err.code] || err.message || errorConsts.Unknown;
-    });
-    return messages?.join("; "); // could parse | errors sent
+    return formatErrorArray(errorData.errors); // could parse | errors sent
   }
+
+  static tryHandleErrorArray(exception: any) {
+    if (exception.constructor !== Array) return undefined;
+    return formatErrorArray(exception);
+  }
+}
+
+function formatErrorArray(errors: BackendError[]) {
+  return errors
+    .map((err) => {
+      return errorMessages[err.code] || err.message || errorConsts.Unknown;
+    })
+    .join("; ");
 }
