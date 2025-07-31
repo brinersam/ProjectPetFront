a4ee650d806beabf4fa6a26aac96adffe0b7fe1f
brinersam <vladbudyachenko@mail.ru>
2025-08-01 00:53:28 +0300

    Подчистка авторизации через axios

--- ProjectPet/src/main.tsx
+++ ProjectPet/src/main.tsx
@@ -4,17 +4,14 @@ import { darkTheme } from "./theme.ts";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/Router.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import AuthProvider from "./context/AuthContext/AuthProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={darkTheme}>
     <Provider store={store}>
-      <AuthProvider>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <CssBaseline />
+      <RouterProvider router={router} />
     </Provider>
   </ThemeProvider>
 );
